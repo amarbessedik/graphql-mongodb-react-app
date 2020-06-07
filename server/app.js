c8e493a68786e1ac973070e701cdc35362ae8ca2
amarbessedik <amar.bessedik@gmail.com>
2020-06-07 15:17:56 -0400

    GraphQL schema created

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+//Add a schema which represents how our graph looks
+// schema: schema //using ES6 becomes simply schema
+schema,
+graphiql:true
+}));
+
 app.listen(4000, ()=>{
     console.log('Now listening for requests on port 4000');
-})
+});
---
+++ server/schema/schema.js
@@ -0,0 +1,99 @@
+//The schema describes the data in the graph with witch we're to interact with
+//The schema serves to mutate and/or add data to the graph
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, 
+        GraphQLString, 
+        GraphQLSchema, 
+        GraphQLID,
+        GraphQLInt,
+        GraphQLList 
+    } = graphql;
+
+//dummy data
+var books = [
+  { name: "Name Of The Wind", genre: "Fantasy", id: "1", authorId: '1' },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: '2' },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id:'4', authorId: '2' },
+  { name: 'The Color of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
+];
+
+const authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" }
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author: {
+        type: AuthorType,
+        resolve(parent, args){
+            console.log(parent);
+            return _.find(authors, {id: parent.authorId});
+        }
+    }
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books:{
+        type: new GraphQLList(BookType),
+        resolve(parent, args){
+            return _.filter(books, {authorId: parent.id});
+        }
+    }
+  }),
+});
+
+//How we first jump into the graph
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from db//other source
+        //_.find(...): from lodash package - npm i lodash
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    },
+
+    books:{
+        type: new GraphQLList(BookType),
+        resolve(parent, args){
+            return books;
+        }
+    },
+    authors:{
+        type: new GraphQLList(AuthorType),
+        resolve(parent, args){
+            return authors;
+        }
+    }
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
